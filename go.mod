62d1af529c23235aaee1c1fe29fb7b834b4056e4
Enrico Speranza <enrico.speranza@gmail.com>
2023-09-21 20:27:48 +0000

    First stub and commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module radar2tacview
+
+go 1.21.1
+
+require (
+	github.com/gocarina/gocsv v0.0.0-20230616125104-99d496ca653d
+	github.com/scylladb/termtables v1.0.0
+)
+
+require (
+	github.com/apcera/termtables v0.0.0-20170405184538-bcbc5dc54055 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gocarina/gocsv"
+	"github.com/scylladb/termtables"
+)
+
+// Version
+const Version = "0.0.1"
+
+// Constans
+const DM = 1.828 //https://en.wikipedia.org/wiki/Data_mile Km
+
+type TargetCSV struct { // Our example struct, you can use "-" to ignore a field
+	TIME  string `csv:"TIME"`
+	NTN   string `csv:"NTN"`
+	ENT   string `csv:"ENT"`
+	X     string `csv:"X"`
+	Y     string `csv:"Y"`
+	SPEED string `csv:"SPEED"`
+	BEAR  string `csv:"BEAR"`
+	ALT   string `csv:"ALT"`
+}
+
+func StringToInt(data string) int {
+	n, _ := strconv.Atoi(data)
+	return n
+}
+
+func IntToString(n int) string {
+	return strconv.Itoa(n)
+}
+
+func Float64ToString(f float64) string {
+	/** converting the f variable into a string */
+	/** 5 is the number of decimals */
+	/** 64 is for float64 type*/
+	return strconv.FormatFloat(f, 'f', 5, 64)
+}
+
+func Float32ToString(f float64) string {
+	/** converting the f variable into a string */
+	/** 5 is the number of decimals */
+	/** 32 is for float32 type*/
+	return strconv.FormatFloat(f, 'f', 5, 32)
+}
+
+func StringToFloat32(data string) float32 {
+	if s, err := strconv.ParseFloat(data, 32); err == nil {
+		return float32(s)
+	} else {
+		return 0.0
+	}
+}
+
+func StringToFloat64(data string) float64 {
+	if s, err := strconv.ParseFloat(data, 64); err == nil {
+		return s
+	} else {
+		return 0.0
+	}
+}
+
+// https://stackoverflow.com/a/37247762
+func round(val float64) int {
+	if val < 0 {
+		return int(val - 0.5)
+	}
+	return int(val + 0.5)
+}
+
+// https://go.dev/play/p/Q-Ufgrw3vZL
+func DDHHMMZ() string {
+	current_time := time.Now().UTC()
+	return fmt.Sprintf("%d%02d%02dZ\n", current_time.Day(), current_time.Hour(), current_time.Minute())
+}
+
+func DDHHMMZmmmYY() string {
+	current_time := time.Now().UTC()
+	return fmt.Sprintf(current_time.Format("021504ZJan06\n"))
+}
+
+func main() {
+	//LoadCSV
+	csvFile, err := os.OpenFile("data/AJ024.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	targets := []*TargetCSV{}
+
+	if err := gocsv.UnmarshalFile(csvFile, &targets); err != nil { // Load targets from file
+		panic(err)
+	}
+
+	table := termtables.CreateTable()
+	table.AddHeaders("X", "Y", "Distance")
+	//Load targets from file and add to list //DEBUG
+	for _, target := range targets {
+		s_X, _ := strconv.ParseFloat(target.X, 64)
+		s_Y, _ := strconv.ParseFloat(target.Y, 64)
+		distancePB := (math.Sqrt(math.Pow(math.Abs(s_X), 2) + math.Pow(math.Abs(s_Y), 2))) * DM
+		table.AddRow(fmt.Sprintf("%f", s_X), fmt.Sprintf("%f", s_Y), fmt.Sprintf("%f", distancePB))
+	}
+	fmt.Println(table.Render())
+}
